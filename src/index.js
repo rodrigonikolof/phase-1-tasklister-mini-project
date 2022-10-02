f84e7eba5a7f6c5a63abe9688ec79b9c43ecd6f8
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-02 16:51:58 +1100

    Added colors based on selected priority, adds element to list, added button to remove element from list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const textbox = document.querySelector('#new-task-description');
+    // console.log(textbox.value);
+    buildToDo(textbox.value);
+  })
 });
+
+function buildToDo(todo){
+  let li = document.createElement('li');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', deleteItem);
+  btn.innerText = 'X';
+  li.innerText = `${todo}  `;
+  document.querySelector('#tasks').appendChild(li);
+  li.appendChild(btn);
+  
+  let select = document.querySelector('#priority');
+  if (select.options[select.selectedIndex].value == 'green'){
+    btn.style.backgroundColor = 'green';
+  } 
+  else if (select.options[select.selectedIndex].value == 'yellow'){
+    btn.style.background = 'yellow';
+  } 
+  else if (select.options[select.selectedIndex].value == 'red'){
+    btn.style.background = 'red';
+  } 
+}
+
+function deleteItem(e){
+  e.target.parentNode.remove();
+}
